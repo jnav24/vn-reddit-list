18593a034d1485875742d4c63a43010decd1f7d4
Justin Navarro <info@justinnavarro.net>
2019-01-13 17:58:55 -0500

    set up routes

--- App.vue
+++ App.vue
@@ -1,70 +1,21 @@
 <template>
-  <view class="container">
-      <text class="text-color-primary">My Vue Native App</text>
-      <TextInput
-          :style="{
-              borderColor: 'grey',
-              borderWidth: 1,
-              height: 40,
-              width: 150,
-          }"
-          v-model="search"/>
-	  <Button
-		  @press="buttonEvent()"
-	  	  title="Click me"
-	      color="#841584"
-	      accessibility-label="Learn more about this button"/>
-	  <TouchableOpacity
-		  class="button"
-		  @press="buttonEvent()">
-		  <Text class="button-text">Login</Text>
-	  </TouchableOpacity>
-
-	  <View v-for="(country, index) in countries" :key="index">
-		  <Text>{{ country.name }}</Text>
-	  </View>
-  </view>
+	<app-navigation></app-navigation>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                search: 'from search',
-				countries: [
-					{ name: 'Japan' },
-					{ name: 'United States' },
-				]
-            }
-        },
-		methods: {
-        	buttonEvent() {
-        		alert('button clicked');
-			},
-		},
-    }
-</script>
+	import { StackNavigator } from 'vue-native-router';
+	import HomeScreen from './src/components/Home.vue';
 
-<style>
-  .container {
-	  align-items: center;
-	  background-color: white;
-	  flex: 1;
-	  justify-content: center;
-  }
-  .button {
-	  background-color: #900;
-	  padding-bottom: 10;
-	  padding-left: 20;
-	  padding-right: 20;
-	  padding-top: 10;
-  }
-  .button-text {
-	  color: #fff;
-  }
-  .text-color-primary {
-	  color: #900;
-	  font-size: 28;
-	  margin-bottom: 20;
-  }
-</style>
+	const AppNavigation = StackNavigator(
+		{
+			Home: HomeScreen,
+		},
+		{
+			initialRouteName: 'Home',
+		}
+	);
+
+	export default {
+		components: { AppNavigation }
+	}
+</script>
